6d5ea6618b07a20adb3f96dc9d9dacffc48d7e10
Haritha <44741104+Harithamol@users.noreply.github.com>
2020-02-13 23:46:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,36 +1,3 @@
-
-//const express = require('express');
-//const path = require('path');
-
-//const app = express();
-
-//app.use(express.static('Harithamol/Quiz-Application/dist/'));
-
-//app.use(express.static(__dirname, 'dist', {index: false}));
-
-//server.listen(port, function() {
-//    console.log("App running on port " + port);
-//})
-
-//app.get('/*', function(req,res) {
-    
-//res.sendFile('./dist/quizapp/index.html');
-//});
-
-
-//app.get('', function(req, res) {
-//    res.sendFile(path.join(__dirname, 'src', 'index.html'));
-//});
-
-//app.get('/', function(req, res) {
-//    res.sendFile(path.join(__dirname, 'src', 'index.html'));
-//});
-
-
-//app.listen(process.env.PORT || 8080);
-
-
-
 const express = require('express');
 const path = require('path');
 
